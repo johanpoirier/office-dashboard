731943c61234b01750af8ac43d1d00b226f01177
Johan Poirier <johan.poirier@gmail.com>
2014-02-17 16:41:09 +0100

    testing html5 desktop notifications

--- public/js/modules/chat/frontend.js
+++ public/js/modules/chat/frontend.js
@@ -23,6 +23,9 @@ define([ "jquery", "socket-io", "handlebars", "hbs!modules/chat/template", "hbs!
             messages.forEach(function (message) {
                 text.append(messageTemplate(message));
             });
+
+            notify(messages[0]);
+
             text.scrollTop(text.prop("scrollHeight"));
         };
 
@@ -40,6 +43,42 @@ define([ "jquery", "socket-io", "handlebars", "hbs!modules/chat/template", "hbs!
                 return false;
             }
         };
+
+        var notify = function(message) {
+            var text = message.author + " : " + message.content;
+
+            // Let's check if the browser supports notifications
+            if (!("Notification" in window)) {
+                alert("This browser does not support desktop notification");
+            }
+
+            // Let's check if the user is okay to get some notification
+            else if (Notification.permission === "granted") {
+                // If it's okay let's create a notification
+                var notification = new Notification(text);
+            }
+
+            // Otherwise, we need to ask the user for permission
+            // Note, Chrome does not implement the permission static property
+            // So we have to check for NOT 'denied' instead of 'default'
+            else if (Notification.permission !== 'denied') {
+                Notification.requestPermission(function (permission) {
+
+                    // Whatever the user answers, we make sure we store the information
+                    if(!('permission' in Notification)) {
+                        Notification.permission = permission;
+                    }
+
+                    // If the user is okay, let's create a notification
+                    if (permission === "granted") {
+                        var notification = new Notification(text);
+                    }
+                });
+            }
+
+            // At last, if the user already denied any notification, and you
+            // want to be respectful there is no need to bother him any more.
+        }
 
         return {
             start: function (config, rootEl) {
