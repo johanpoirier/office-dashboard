18bae0294cc405618b53433a824191dc14de2abe
Johan Poirier <johan.poirier@gmail.com>
2014-02-17 16:53:51 +0100

    auto closing notifications

--- public/js/modules/chat/frontend.js
+++ public/js/modules/chat/frontend.js
@@ -45,39 +45,45 @@ define([ "jquery", "socket-io", "handlebars", "hbs!modules/chat/template", "hbs!
         };
 
         var notify = function(message) {
-            var text = message.author + " : " + message.content;
+            if(message) {
+                var notification;
+                var text = message.author + " : " + message.content;
 
-            // Let's check if the browser supports notifications
-            if (!("Notification" in window)) {
-                alert("This browser does not support desktop notification");
+                // Let's check if the browser supports notifications
+                if (!("Notification" in window)) {
+                    alert("This browser does not support desktop notification");
+                }
+
+                // Let's check if the user is okay to get some notification
+                else if (Notification.permission === "granted") {
+                    // If it's okay let's create a notification
+                    notification = new Notification(text);
+                }
+
+                // Otherwise, we need to ask the user for permission
+                // Note, Chrome does not implement the permission static property
+                // So we have to check for NOT 'denied' instead of 'default'
+                else if (Notification.permission !== 'denied') {
+                    Notification.requestPermission(function (permission) {
+
+                        // Whatever the user answers, we make sure we store the information
+                        if(!('permission' in Notification)) {
+                            Notification.permission = permission;
+                        }
+
+                        // If the user is okay, let's create a notification
+                        if (permission === "granted") {
+                            notification = new Notification(text);
+                        }
+                    });
+                }
+
+                if(notification) {
+                    setTimeout(function() {
+                        notification.close();
+                    }, 5000);
+                }
             }
-
-            // Let's check if the user is okay to get some notification
-            else if (Notification.permission === "granted") {
-                // If it's okay let's create a notification
-                var notification = new Notification(text);
-            }
-
-            // Otherwise, we need to ask the user for permission
-            // Note, Chrome does not implement the permission static property
-            // So we have to check for NOT 'denied' instead of 'default'
-            else if (Notification.permission !== 'denied') {
-                Notification.requestPermission(function (permission) {
-
-                    // Whatever the user answers, we make sure we store the information
-                    if(!('permission' in Notification)) {
-                        Notification.permission = permission;
-                    }
-
-                    // If the user is okay, let's create a notification
-                    if (permission === "granted") {
-                        var notification = new Notification(text);
-                    }
-                });
-            }
-
-            // At last, if the user already denied any notification, and you
-            // want to be respectful there is no need to bother him any more.
         }
 
         return {
