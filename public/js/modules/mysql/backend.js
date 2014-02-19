e59523e174d49a4965e98b63a29d2b08b6e66799
Julien Colin <julien.colin@atos.net>
2014-02-19 09:44:12 +0100

    css for [mysql] module

--- public/js/modules/mysql/backend.js
+++ public/js/modules/mysql/backend.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 
-var config, iosockets;
+var _config, _iosockets;
 var connection;
 
 exports.withConfig = function(cfg) {
     console.log("[mysql] module loaded");
-    config = cfg;
+    _config = cfg;
     connection = mysql.createConnection({
-      host     : config['host'],
-      user     : config['user'],
-      password : config['password'],
-      database : config['database'],
+      host     : _config['host'],
+      user     : _config['user'],
+      password : _config['password'],
+      database : _config['database'],
       dateStrings : "true"
     });
     return this;
@@ -21,23 +21,37 @@ exports.start = function(socketio) {
         if (err) {
            throw err; 
        } else {
-            console.log("[mysql] connection to mysql db established : " + config['host']);
+            console.log("[mysql] connection to mysql db established : " + _config['host']);
        }
     });
     
     var mysqlModule = this;
-    iosockets = socketio;
-    iosockets.on('connection', function (socket) {
+    _iosockets = socketio;
+    _iosockets.on('connection', function (socket) {
         socket.on("mysql:screen", getData.bind(mysqlModule));
     });
-    setInterval(getData.bind(this), config['refresh']);
+    setInterval(getData.bind(this), _config['refresh']);
 }
 
 exports.getLatestBusinessMessages = function(callback) {
-    if(config['database'] != '' && config['table'] != '') {
+    if(_config['database'] != '' && _config['table'] != '') {
         var businessMessages = [];
 
-        connection.query('SELECT * FROM ' + config['table'] + " order by " + config['date_field'] + " desc limit " +  config['fetched_items'] , function(err, results) {
+        var fields = [];
+        _config['fields'].forEach(function(field) {
+            fields.push(field.field_table_name);
+        });
+
+        // Construct select query
+        var queryString = 'SELECT ' + fields.join(',') + ' FROM ' + _config['table'];
+        if(_config['query']){
+            if(_config['query']['orderBy_fields']) queryString += ' ORDER BY ' + _config['query']['orderBy_fields'].join(',');
+            if(_config['query']['orderBy_fields'] && _config['query']['orderBy_sort']) queryString += ' ' + _config['query']['orderBy_sort'];
+        }
+        queryString += ' LIMIT ' + _config['fetched_items'];
+
+        // Play query
+        connection.query(queryString , function(err, results) {
             if(typeof err !== "undefined") {
                 businessMessages = results;
                 console.info("[mysql] " + businessMessages.length + " business messages - " + new Date());
@@ -57,6 +71,6 @@ exports.getLatestBusinessMessages = function(callback) {
 
 var getData = function() {
     this.getLatestBusinessMessages(function(businessMessages) {
-        iosockets.emit("mysql:businessMessage", businessMessages);
+        _iosockets.emit("mysql:businessMessage", businessMessages);
     });
 };
