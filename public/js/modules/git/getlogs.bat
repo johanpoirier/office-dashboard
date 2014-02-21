85c4e744987e99c9973617278ac1178a4ac5dc4d
Johan Poirier <johan.poirier@gmail.com>
2014-02-21 22:38:06 +0100

    if git repo already fetched, fetch without depth

--- public/js/modules/git/getlogs.bat
+++ public/js/modules/git/getlogs.bat
@@ -5,11 +5,14 @@ CD %1
 IF NOT EXIST %2 mkdir %2
 CD %2
 
-IF NOT EXIST .git git init
 IF NOT %4=="" git config http.proxy %4
-
-git remote add origin %3
-git fetch --depth=%6 -n
+IF NOT EXIST .git (
+    git init
+    git remote add origin %3
+    git fetch --depth=%6 -n
+) ELSE (
+    git fetch
+)
 git log --pretty=format:"%%an;%%s" --no-merges %5 > logs
 
 CD %1
--- public/js/modules/git/getlogs.sh
+++ public/js/modules/git/getlogs.sh
@@ -4,16 +4,18 @@ if [ ! -d $2 ]; then
 fi
 cd $2
 
-if [ ! -d .git ]; then
-    git init
-fi
-
 if [ ! $4 = "" ]; then
     git config http.proxy $4
 fi
 
-git remote add origin %3
-git fetch --depth=$6 -n
-git log --pretty=format:"%%an;%%s" --no-merges $5 > logs
+if [ ! -d .git ]; then
+    git init
+    git remote add origin $3
+    git fetch --depth=$6 -n
+else
+    git fetch
+fi
+
+git log --pretty=format:"%an;%s" --no-merges $5 > logs
 
 cd $1
