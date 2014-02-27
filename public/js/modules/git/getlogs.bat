f0b8c8a1c9cf3e2951acfd8a337f9688d6a7a8d0
Johan Poirier <johan.poirier@gmail.com>
2014-02-27 11:23:36 +0100

    Fix git conf proxy setting

--- public/js/modules/git/getlogs.bat
+++ public/js/modules/git/getlogs.bat
@@ -5,12 +5,13 @@ CD %1
 IF NOT EXIST %2 mkdir %2
 CD %2
 
-IF NOT %4=="" git config http.proxy %4
 IF NOT EXIST .git (
     git init
     git remote add origin %3
+    IF NOT %4=="" git config http.proxy %4
     git fetch --depth=%6 -n
 ) ELSE (
+    IF NOT %4=="" git config http.proxy %4
     git fetch
 )
 git log --pretty=format:"%%an;%%s;%%ar" --no-merges %5 > logs
