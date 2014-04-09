093454280b2a75bd75e8c7915e84fb77ef2dea3b
Johan Poirier <johan.poirier@gmail.com>
2014-04-09 14:14:49 +0200

    testing travis ci

--- public/js/modules/git/backend.js
+++ public/js/modules/git/backend.js
@@ -26,10 +26,11 @@ var GitModule = OfficeModule.extend({
                 var urlParts = url.split("//");
                 url = urlParts[0] + "//" + this.config["user"] + ":" + this.config["password"] + "@" + urlParts[1];
             }
-
             var cmd = [ __dirname, '/getlogs.', (this.isWin ? 'bat' : 'sh' ), ' "', this.globalConfig["tempDir"], '" "', this.config["repo"],
                 '" ', url, ' "', (this.proxy && !this.proxy.bypass(this.config["url"]) ? this.proxy["url"] : ""), '" ',
                 this.config["branch"], ' ', 50 ];
+
+            console.log(cmd.join(""));
 
             exec(cmd.join(""),
                 (function (error, stdout, stderr) {
