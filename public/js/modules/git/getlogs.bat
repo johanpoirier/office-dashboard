90dd47f9804a4873da78518c581e01b544dfdb23
Johan Poirier <johan.poirier@gmail.com>
2014-05-07 16:18:20 +0200

    remove --local to git commands

--- public/js/modules/git/getlogs.bat
+++ public/js/modules/git/getlogs.bat
@@ -8,12 +8,12 @@ CD %2
 IF NOT EXIST .git (
     git init
     git remote add origin %3
-    IF NOT %4=="" git config --local http.proxy %4
-    IF %4=="" git config --local --unset http.proxy
+    IF NOT %4=="" git config http.proxy %4
+    IF %4=="" git config --unset http.proxy
     git fetch --depth=%6 -n
 ) ELSE (
-    IF NOT %4=="" git config --local http.proxy %4
-    IF %4=="" git config --local --unset http.proxy
+    IF NOT %4=="" git config http.proxy %4
+    IF %4=="" git config --unset http.proxy
     git remote remove origin
     git remote add origin %3
     git fetch
